f34639eb49341ba6546460facaab0294accc55a6
Jenny Tan <tanjenny.yo@gmail.com>
2020-09-30 23:58:20 +0000

    fixed parameters for functions

--- calculator.py
+++ calculator.py
@@ -3,29 +3,28 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
-
-While True:
+while True:
     command = input ("Please input your calculation, starting with function(add, subtract, multiply, divide, square, cube, power, mod), followed by the two numbers, all separated by spaces")
     command_list = command.split(' ')
     which_function = command_list[0]
-    num1 = command_list[1]
-    num2 = command_list[2] 
+    num1 = int(command_list[1])
+    num2 = int(command_list[2]) 
 
     if which_function == "q":
         break
     elif which_function == "add":
-        return add(num1,num2)
+        print (add(num1,num2))
     elif which_function == "subtract":
-        return subtract(num1,num2)
+        print (subtract(num1,num2))
     elif which_function == "multiply":
-        return multiply(num1,num2)
+        print (multiply(num1,num2))
     elif which_function == "divide":
-        return divide(num1,num2)
+        print (divide(num1,num2))
     elif which_function == "square":
-        return square(num1,num2)
+        print (square(num1))
     elif which_function == "cube":
-        return cube(num1,num2)
+        print (cube(num1))
     elif which_function == "power":
-        return power(num1,num2)
+        print (power(num1,num2))
     elif which_function == "mod":
-        return mod(num1,num2)
+        print (mod(num1,num2))
