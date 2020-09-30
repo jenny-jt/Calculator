f64d8c0e3d5ebcd9af16dc1bfc0b22a951af4667
Jenny Tan <tanjenny.yo@gmail.com>
2020-09-30 23:43:24 +0000

    defined variables

--- calculator.py
+++ calculator.py
@@ -4,4 +4,10 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
 
-# Replace this with your code
+While True:
+    command = input ("Please input your calculation, starting with function(add, subtract, ultiply, divide, square, cube, power, mod), followed by the two numbers, all separated by spaces")
+    command_list = command.split(' ')
+    which_function = command_list[0]
+    num1 = command_list[1]
+    num2 = command_list[2] 
+    
